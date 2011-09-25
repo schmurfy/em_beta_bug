358425fad403d23950bdc9d739f96494ef798c58
Julien Ammous <j.ammous@gmail.com>
2011-09-25 15:54:43 +0200

    added a dummy controller making an http request

--- Gemfile
+++ Gemfile
@@ -2,9 +2,8 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
 
-# Bundle edge Rails instead:
-# gem 'rails',     :git => 'git://github.com/rails/rails.git'
-
+gem 'em-http-request'
+gem "thin"
 
 
 # Gems used only for assets and not required
@@ -16,13 +15,4 @@ group :assets do
 end
 
 gem 'jquery-rails'
-
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
 
---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,48 @@
+class MainController < ApplicationController
+  def index
+    http = load_page('http://google.com')
+    render :text => "Done"
+  end
+
+private
+  def load_page(url)
+    th = Thread.current
+    
+    if th == EM::reactor_thread
+      raise "Trying to issue an HTTP Request from the EventMachine thread, aborting..."
+    end
+    
+    # uri = URI.parse(url)
+    # http = EventMachine::Protocols::HttpClient.request(
+    #     :host => uri.host,
+    #     :port => uri.port,
+    #     :request => uri.path
+    #   )
+    
+    http = EM::HttpRequest.new(url).get(:head => {
+        'user-agent'  => "Mac FireFox",
+        'cookie'      => 'birthtime=-2208959999' # this takes care of the age check
+      })
+
+    http.callback do
+      Rails.logger.debug("callback, waking up thread #{'%#x' % Thread.current.object_id}")
+      th.wakeup
+    end
+    
+    http.errback do
+      Rails.logger.debug("errback, waking up thread #{'%#x' % Thread.current.object_id}")
+      th.wakeup
+    end
+    
+    Rails.logger.debug("[#{'%#x' % Thread.current.object_id}]Thread suspended")
+    sleep
+    
+    Rails.logger.debug("[#{'%#x' % Thread.current.object_id}]Thread woken up")
+        
+    if http.error
+      raise PageLoadFailed, http
+    end
+    
+    http
+  end
+end
